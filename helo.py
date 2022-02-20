cb1902141aea47cb29ebbf7c995fd5d79e82ddb3
githubanhduong <20521225@gm.uit.edu.vn>
2022-02-20 15:26:37 +0700

    init

--- helo.py
+++ helo.py
@@ -4,11 +4,11 @@ array = [1, 2, 3, 4, 5]
 shift = 2
 
 for i in range(0, shift):
-    temp = array[0]
+    tem = array[0]
 
     for j in range(0, len(array) - 1):
         array[j] = array[j + 1]
-    array[len(array) - 1] = temp
+    array[len(array) - 1] = tem
 
 for i in range(0, len(array)):
     print(array[i])
